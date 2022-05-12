b688fdb9d7fcf0f276f025789070328772b7f171
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-05-12 19:36:24 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,27 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+// Copyright 2022 NNTU-CS
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
 
 template<typename T, int size>
-class TStack {
-
+class TPQueue {
+private:
+T ar[100];
+int k, m;
+public:
+TPQueue() : k(0), m(0) {}
+void push(T znach) {
+int i = m++;
+while ((--i >= k) && (ar[i % size].prior < znach.prior)) {
+ar[(i + 1) % size] = ar[i % size];
+}
+ar[(i + 1) % size] = znach;
+}
+T pop() {
+return ar[(k++) % size];
+}
 };
-#endif  // INCLUDE_TSTACK_H_
+struct SYM {
+  char ch;
+  int prior;
+};
+#endif  // INCLUDE_TPQUEUE_H_
