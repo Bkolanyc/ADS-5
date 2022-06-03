dabed7ade00a981b1266d10b2bb8c5c137077132
Николай Бутусов <99186896+Bkolanyc@users.noreply.github.com>
2022-06-03 10:41:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,111 @@
+
 // Copyright 2021 NNTU-CS
 #include <string>
-#include <map>
 #include "tstack.h"
-
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int priority(char a) {
+switch (a) {
+case '(': return 0;
+case ')': return 1;
+case '+': return 2;
+case '-': return 2;
+case '*': return 3;
+case '/': return 3;
+default: return -1;
 }
-
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+}
+std::string infx2pstfx(std::string inf) {
+    char head = 0;
+    TStack <char, 100> stack1;
+    std::string str;
+    for (int i = 0; i < inf.length() ; i++) {
+        int prior;
+        prior = priority(inf[i]);
+        if (prior > -1) {
+            if ((prior == 0 || prior > priority(head) ||
+                 stack1.isEmpty()) && inf[i] != ')') {
+                if (stack1.isEmpty())
+                    head = inf[i];
+                stack1.push(inf[i]);
+            } else if (inf[i] == ')') {
+                while (stack1.get() != '(') {
+                    str.push_back(stack1.get());
+                    str.push_back(' ');
+                    stack1.pop();
+                }
+                stack1.pop();
+                if (stack1.isEmpty())
+                    head = 0;
+            } else {
+                while (!stack1.isEmpty() &&
+                       priority(stack1.get()) >= prior) {
+                    str.push_back(stack1.get());
+                    str.push_back(' ');
+                    stack1.pop();
+                }
+                if (stack1.isEmpty())
+                    head = inf[i];
+                stack1.push(inf[i]);
+            }
+        } else {
+            str.push_back(inf[i]);
+            str.push_back(' ');
+        }
+    }
+    while (!stack1.isEmpty()) {
+        str.push_back(stack1.get());
+        str.push_back(' ');
+        stack1.pop();
+    }
+    str.erase(str.end() - 1, str.end());
+    return str;
+}
+int calculator(char operate, int x, int y) {
+  switch (operate) {
+      case '+':
+          return x + y;
+          break;
+      case '-':
+          return x - y;
+          break;
+      case '*':
+          return x * y;
+          break;
+      case '/':
+          return x / y;
+          break;
+  }
+    return 0;
+}
+int eval(std::string pst) {
+TStack <int, 100> stack2;
+int i = 0, result = 0;
+char ch = pst[i];
+while (ch) {
+if (ch >= '0' && ch <= '9') {
+int chislo = 0;
+int dec = 1;
+while (ch != ' ') {
+    chislo += (ch - 48) * dec;
+dec *= 10;
+ch = pst[++i];
+}
+stack2.push(chislo);
+} else {
+char operate = ch;
+i++;
+int number2 = stack2.get();
+stack2.pop();
+int number1 = stack2.get();
+stack2.pop();
+int result = calculator(operate, number1, number2);
+stack2.push(result);
+}
+if (i < pst.size())
+ch = pst[++i];
+else
+ch = 0;
+}
+result = stack2.get();
+stack2.pop();
+return result;
 }
